e0548b40eb2dcee253f785ce40399fcdfe8c0e79
ChoudhryAwais <awaischoudhry306@gmail.com>
2025-01-28 16:40:12 +0500

    textual change

--- src/components/LeaseGeneralInfo/LeaseGeneralInfoForm.jsx
+++ src/components/LeaseGeneralInfo/LeaseGeneralInfoForm.jsx
@@ -292,7 +292,7 @@ export default function LeaseGeneralInfoForm({ otherTabs, increment }) {
                                     id="increment"
                                     name="increment"
                                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    placeholder="Enter Incremental amount"
+                                    placeholder="Enter Incremental value in %"
                                     value={formData.increment || ""}
                                     onChange={handleChange}
                                 />
