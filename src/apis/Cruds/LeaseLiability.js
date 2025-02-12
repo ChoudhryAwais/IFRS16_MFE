a416b85470ad23d2ab01cecee945f1e372c17501
ChoudhryAwais <awaischoudhry306@gmail.com>
2025-02-12 21:06:44 +0500

    Change the Stratergy of FC

--- src/apis/Cruds/LeaseLiability.js
+++ src/apis/Cruds/LeaseLiability.js
@@ -1,11 +1,8 @@
 import { callApi } from "../callApi"
-import { getCompanyProfile } from "./sessionCrud"
 
 export const getLeaseLiabilityForLease = async (pageNumber, pageSize, selectedLease) => {
-    const companyProfile = getCompanyProfile()
-    let fc_lease = selectedLease.currencyID === companyProfile.reportingCurrencyId ? 0 : 1
     try {
-        const response = await callApi(`/LeaseLiability?pageNumber=${pageNumber}&pageSize=${pageSize}&leaseId=${selectedLease.leaseId}&fc_lease=${fc_lease}`, "GET")
+        const response = await callApi(`/LeaseLiability?pageNumber=${pageNumber}&pageSize=${pageSize}&leaseId=${selectedLease.leaseId}`, "GET")
         return response
     } catch (error) {
         return error
