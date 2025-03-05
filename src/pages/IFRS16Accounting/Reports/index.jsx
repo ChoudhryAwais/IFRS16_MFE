72445f1894d858f18c0674abce81dc5d0e0806b2
ChoudhryAwais <awaischoudhry306@gmail.com>
2025-03-05 21:11:10 +0500

    Refactor Reports component to use LeaseReport for improved structure and readability

--- src/pages/IFRS16Accounting/Reports/index.jsx
+++ src/pages/IFRS16Accounting/Reports/index.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import { leaseReportCol } from '../../../utils/tableCols/tableCols'
 import { handleExcelExport } from '../../../utils/exportService/excelExportService'
-import { JEReportExcelCol, leaseReportExcelCol } from '../../../utils/tableCols/tableColForExcelExport'
-import Tables from '../../../components/Tables/Tables'
 import Tabs from '../../../components/Tabs/Tabs'
 import JEReport from './JEReport'
+import LeaseReport from './LeaseReport'
 
 export default function Reports({ report }) {
 
@@ -24,27 +22,7 @@ export default function Reports({ report }) {
             id: '1',
             label: 'Lease Report',
             component: (
-                <div>
-                    <div className='text-right'>
-                        {report?.leaseReport.length !== 0 ?
-                            <button
-                                onClick={() => handleExport(report?.leaseReport, leaseReportExcelCol, "Lease Report")}
-                                type="button"
-                                className={" py-2 px-3 mb-2 text-sm font-sm text-white focus:outline-none bg-green-600  rounded-sm border border-gray-200 hover:bg-green-700 hover:text-white "}
-                                disabled={report?.leaseReport.length == 0}
-                            >
-                                Export <i class="fa fa-download ml-2"></i>
-                            </button> : null}
-                    </div>
-                    <Tables
-                        data={report?.leaseReport || []}
-                        columns={leaseReportCol}
-                        calcHeight="150px"
-                        isLoading={report.loading}
-                        totalRecord={report.totalRecord}
-                        pagination={false}
-                    />
-                </div>
+                <LeaseReport data={report?.leaseReport} loading={report.loading} handleExport={handleExport} />
             ),
         },
         {
