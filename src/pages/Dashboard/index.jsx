f98c3bb637bc11cd1ce44420d6915c17b9a359b3
ChoudhryAwais <awaischoudhry306@gmail.com>
2025-05-15 16:26:20 +0500

    Total Leases added

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -40,7 +40,7 @@ export default function Dashboard() {
             <div className='mb-5'>
                 <DashboardCards allLeases={allLeases} />
             </div>
-            {/* <CollapsibleFilterBox heading="Report">
+            <CollapsibleFilterBox heading="Report">
                 <GeneralFilter
                     onApplyFilter={(filterModal) => aggregationForLease(filterModal)}
                     showLeaseSelection={true}
@@ -59,7 +59,7 @@ export default function Dashboard() {
                     totalRecord={leaseReport.totalRecord}
                     pagination={false}
                 />
-            </div> */}
+            </div>
         </div>
     )
 }
