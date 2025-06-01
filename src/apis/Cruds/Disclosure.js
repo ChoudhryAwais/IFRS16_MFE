364337c655beb4df2c8cb123634752c61cf0bf7a
ChoudhryAwais <awaischoudhry306@gmail.com>
2025-06-01 19:55:01 +0500

    Disclosure Fixed

--- src/apis/Cruds/Disclosure.js
+++ src/apis/Cruds/Disclosure.js
@@ -1,6 +1,9 @@
 import { callApi } from "../callApi"
+import { getCompanyProfile } from "./sessionCrud"
 
 export const getDisclosureReport = async (filter) => {
-    const response = await callApi(`/Disclosure/Get`, "POST", filter)
+    const companyProfile = getCompanyProfile()
+    const payload = { ...filter, companyId: companyProfile.companyID }
+    const response = await callApi(`/Disclosure/Get`, "POST", payload)
     return response
 }
